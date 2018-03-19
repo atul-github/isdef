0b8ace108afd1bc5adefd83cf22e6b46f2eaa1c1
Atul <atul_pandit@yahoo.com>
2018-03-19 19:57:17 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,9 +4,8 @@ var isdef = require('../');
 var x={};
 x.y={};
 x.y.z={};
-x.y.z.a=function(){};
+x.y.z.a="Hi there";
 
 assert (isdef(x, "y.z.a") !== undefined, "Test Failed!!")
 assert (isdef(x, "y.z.a.m") === undefined, "Test Failed!!")
-
-
+assert (typeof(isdef(x, "y.z.a")) === 'string', "Test Failed!!")
